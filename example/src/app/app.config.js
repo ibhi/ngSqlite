8945a02498022bb3bcaafd11a21978be4c8240d3
ibhi <ibrahimin2004@gmail.com>
2016-09-17 21:49:37 +0530

    Done: Routing

--- example/src/app/app.config.js
+++ example/src/app/app.config.js
@@ -3,13 +3,28 @@
 	angular.module('app')
 		.config(appConfig);
 
-	appConfig.$inject = ['cacheProvider'];
+	appConfig.$inject = ['cacheProvider', '$stateProvider', '$urlRouterProvider'];
 
-	function appConfig(cacheProvider) {
+	function appConfig(cacheProvider, $stateProvider, $urlRouterProvider) {
 		var cfg = {
 		    dbName: 'app',
 		    dbLocation:'default'
 	    }
 		cacheProvider.config(cfg);
+		$urlRouterProvider.otherwise('/');
+		$stateProvider.state('create', {
+			url: '/',
+			template: '<create-table table="vm.table"></create-table>'
+		})
+		.state('insert', {
+			url: '/insert',
+			template: '<insert-record table="vm.table"></insert-record>'
+		})
+		.state('update', {
+			url: '/update',
+			template: '<update-record table="vm.table"></update-record>'
+		})
+
+
 	}
 })()
--- example/src/app/app.module.js
+++ example/src/app/app.module.js
@@ -1,4 +1,4 @@
 (function() {
 	'use strict';
-	angular.module('app', ['ngSqlite']);
+	angular.module('app', ['ngSqlite', 'ui.router']);
 })()
